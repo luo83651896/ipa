43d753b9dc8177500cb4a79a930800fb9823f92c
luo83651896 <31127597+luo83651896@users.noreply.github.com>
2018-05-10 00:35:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,26 @@
 var http = require("http");  
-const child_process = require('child_process');
+var url=require("url");
 server = http.createServer(onRequest);  
-server.listen(process.env.PORT || 80);  
-function onRequest(request, response){  
-        child_process.exec('chmod 777 ./frps', function (error, stdout, stderr) {
-        if (error) {
-            console.log(error.stack);
-            console.log('错误码: '+error.code);
-            console.log('接收到: '+error.signal);
+server.listen(process.env.PORT || 800);  
+//var aa="aaaa";
+//console.log("bbb"+aa+"ccc");
+//console.log("测试");
+function onRequest(req, res){ 
+	    var urls=url.parse(req.url, true);
+	    if (urls.path.match(/\.plist/g))
+	    {
+	    	var name=urls.path.slice(1,-6);
+	    	//console.log(name);
+	    	var itms="<?xml version=\"1.0\" encoding=\"UTF-8\"?><!DOCTYPE plist PUBLIC \"-//Apple//DTD PLIST 1.0//EN\" \"http://www.apple.com/DTDs/PropertyList-1.0.dtd\"><plist version=\"1.0\"><dict><key>items</key><array><dict><key>assets</key><array><dict><key>kind</key><string>software-package</string><key>url</key><string>http://127.0.0.1/install/"+name+".ipa</string></dict></array><key>metadata</key><dict><key>bundle-identifier</key><string>*</string><key>bundle-version</key><string>1.0.0</string><key>kind</key><string>software</string><key>title</key><string>免Appstore安装项目</string></dict></dict></array></dict></plist>";
+	    	res.writeHead(200, {'Content-Type': 'text/plain;charset=utf-8'});
+	    	res.write(itms);
+	    	res.end(); 
+	    }
+        else
+        {
+        	res.writeHead(404, {'Content-Type': 'text/plain'});
+        	res.end(); 
         }
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-    });
-        var workerProcess = child_process.exec('./frps -c ./frps.ini', function (error, stdout, stderr) {
-        if (error) {
-            console.log(error.stack);
-            console.log('Error code: '+error.code);
-            console.log('Signal received: '+error.signal);
-        }
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-    });
-        workerProcess.on('exit', function (code) {
-        console.log('子进程已退出，退出码 '+code);
-    });
-        response.writeHead(200, {'Content-Type': 'text/plain'});  
-        response.end('Hello World post\n');   
-        console.log('this ok!!!!');
+        //console.log(urls);
+          
 }  
