d3aba3dc1c2f65d351552cf357b569e023b38f41
luo83651896 <31127597+luo83651896@users.noreply.github.com>
2018-05-09 21:31:04 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 var http = require("http");
-//设置主机名
-var hostName = '127.0.0.1';
 //设置端口
 var port = 80;
 //创建服务
@@ -8,7 +6,4 @@ var server = http.createServer(function(req,res){
     res.setHeader('Content-Type','text/plain');
     res.end("hello nodejs");
 
-});
-server.listen(port,hostName,function(){
-    console.log(`服务器运行在http://${hostName}:${port}`);
-});
+}).listen(port);
