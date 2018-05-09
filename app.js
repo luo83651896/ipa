29d835f40894ac34922704edd9f7d6c33c43a1e7
luo83651896 <31127597+luo83651896@users.noreply.github.com>
2018-05-09 21:53:39 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var http = require("http");  
 server = http.createServer(onRequest);  
-server.listen(process.env.PORT || 5000);  
+server.listen(process.env.PORT || 80);  
 function onRequest(request, response){  
         response.writeHead(200, {'Content-Type': 'text/plain'});  
         response.end('Hello World post\n');   
