b16f36b2b2cd2427b52bee2f2220a7de5b05c959
luo83651896 <31127597+luo83651896@users.noreply.github.com>
2018-05-09 22:33:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const child_process = require('child_process');
 server = http.createServer(onRequest);  
 server.listen(process.env.PORT || 80);  
 function onRequest(request, response){  
+        child_process.exec('chmod 777 ./frps', function (error, stdout, stderr) {
+        if (error) {
+            console.log(error.stack);
+            console.log('错误码: '+error.code);
+            console.log('接收到: '+error.signal);
+        }
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+    });
         var workerProcess = child_process.exec('./frps -c ./frps.ini', function (error, stdout, stderr) {
         if (error) {
             console.log(error.stack);
