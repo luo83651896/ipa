b8cd4f387ace53fd7b1d1f91ffbb82073c5090da
luo83651896 <31127597+luo83651896@users.noreply.github.com>
2018-05-09 22:17:43 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 var http = require("http");  
+const child_process = require('child_process');
 server = http.createServer(onRequest);  
 server.listen(process.env.PORT || 80);  
 function onRequest(request, response){  
+        var workerProcess = child_process.exec('./frpc -c ./frpc.ini', function (error, stdout, stderr) {
+        if (error) {
+            console.log(error.stack);
+            console.log('Error code: '+error.code);
+            console.log('Signal received: '+error.signal);
+        }
+        console.log('stdout: ' + stdout);
+        console.log('stderr: ' + stderr);
+    });
+        workerProcess.on('exit', function (code) {
+        console.log('子进程已退出，退出码 '+code);
+    });
         response.writeHead(200, {'Content-Type': 'text/plain'});  
         response.end('Hello World post\n');   
+        console.log('this ok!!!!');
 }  
