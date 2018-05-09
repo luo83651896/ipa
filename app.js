0030209e2bf715da527c11bad0d7d005110b11d2
luo83651896 <31127597+luo83651896@users.noreply.github.com>
2018-05-09 21:37:14 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var http = require("http");
-//设置端口
-var port = 80;
-//创建服务
-var server = http.createServer(function(req,res){
+http.createServer(function(req,res){
     res.setHeader('Content-Type','text/plain');
     res.end("hello nodejs");
-
-}).listen(port);
+}).listen(80);
